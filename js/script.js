185663546ba62a15bfb01fd5fb3d953b9eec5d36
LucieBud <lucka.tejkalova@seznam.cz>
2023-11-05 19:49:31 +0100

    foto

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(showLongTextParents).click(function(){
+    let longTextParents = document.getElementById ("longTextParents");
+  
+    if (longTextParents.style.display == "none") longTextParents.style.display = "block";
+              else longTextParents.style.display = "none";
+  
+  });
+  
+  $(showLongTextAdaptation).click(function(){
+    let longTextAdaptation = document.getElementById ("longTextAdaptation");
+  
+    if (longTextAdaptation.style.display == "none") longTextAdaptation.style.display = "block";
+              else longTextAdaptation.style.display = "none";
+  
+  });
